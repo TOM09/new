c9d983fba7b3faef2d16419f93128dfe9054f243
tom0209 <songhua0209@foxmail.com>
2017-10-31 23:15:22 +0800

    @text

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+	//入口  惯例用main.js
+	entry: "./app/main.js", // string | object | array
+	//出口  惯例用all.js
+	output: {
+		path: path.resolve(__dirname, "dist"), // string
+
+		filename: "all.js" // string
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['es2015','react']
+				}
+			}
+		}]
+	},
+	watch:true
+}
